b49749c114a415a77edc9e7628545b10637a70b5
Anand.C <anandchannazhi01@gmail.com>
2023-03-14 01:01:56 +0530

    changed few files
    removed photo
    added one file
    modified 2 files

--- forms/contact.php
+++ forms/contact.php
@@ -2,17 +2,19 @@
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * The library this is a change be uploaded to: vendor/php-email-form/php-email-form.php
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
+  //adding this line
+
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'contact@dddddd.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Udfsfds to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
